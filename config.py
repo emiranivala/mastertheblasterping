76ce2c935bd94a798dbb2e22787f2d15f13d20fd
emiranivala <taitufam@gmail.com>
2025-03-23 13:57:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,17 @@
-# devgagan
-# Note if you are trying to deploy on vps then directly fill values in ("")
-
 from os import getenv
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-API_ID = int(getenv("API_ID", "0"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "22642292"))
+API_HASH = getenv("API_HASH", "4502d35191a2fcb02c8467f54789f0ea")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, filter(None, getenv("OWNER_ID", "").split())))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://crushe:crushe123@cluster0.rlmxpqw.mongodb.net/?retryWrites=true&w=majority")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "0"))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "-1002230414810")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002304203111"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "20"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL")
-AD_API = getenv("AD_API")
-STRING = getenv("STRING")
-YT_COOKIES = getenv("YT_COOKIES")
-INSTA_COOKIES = getenv("INSTA_COOKIES")
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500000000000000000"))
+WEBSITE_URL = getenv("WEBSITE_URL", None)
+AD_API = getenv("AD_API", None)
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", None)
+INSTA_COOKIES = getenv("INSTA_COOKIES", None)
 SECONDS = 300  # for example, a 5-minute delay
